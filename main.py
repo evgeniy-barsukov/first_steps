de5bb935a8be002cb66a2fea5eb2d178ee30d28b
Unweldify <AILALAH@yandex.ru>
2024-09-27 18:16:19 +0400

    Changed variable names to snake_case Added type annotations for function arugments and returns Cleaned up spacing of the code

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-def fibonacciSeq(n):
+def get_fibonacci(n: int) -> int:
     if (n == 1) or (n == 0):
         return n
     else:
-        return fibonacciSeq(n - 1) + fibonacciSeq(n - 2)
+        return get_fibonacci(n - 1) + get_fibonacci(n - 2)
 
-def isPrime(n, i = 2):
+
+def is_prime(n: int, i=2) -> bool:
     if n <= 2:
         return True if n == 2 else False
     if (n % i) == 0:
         return False
     if (i * i) > n:
         return True
-    
-    return isPrime(n, i + 1)
 
-# 
+    return is_prime(n, i + 1)
 
-with open("in_nums.txt", "r") as inFile:
-    nums = inFile.read().split()
-    inFile.close()
 
-with open("out_nums.txt", "w") as outFile:
-    for num in nums:
-        lastDigit = int(num[-1])
-        num = int(num)
+with open("in_nums.txt", "r") as in_file:
+    numbers = in_file.read().split()
+    in_file.close()
 
-        if isPrime(num):
-            outFile.write(f'{fibonacciSeq(lastDigit)}\n')
-    outFile.close()
+with open("out_nums.txt", "w") as out_file:
+    for number in numbers:
+        last_digit = int(number[-1])
+        number = int(number)
 
-    
+        if is_prime(number):
+            out_file.write(f'{get_fibonacci(last_digit)}\n')
+    out_file.close()
