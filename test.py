a857cbdaa42e3821fbafaf534788dad7355d660e
Varvara <varya.shevaldova@mail.ru>
2024-09-27 18:18:35 +0400

    lesson1

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-
 # число фибоначчи
-def fibFunc(num):
-    if num<=1:
+def fib_func(num):
+    if num <= 1:
         return num
     else:
-        return  fibFunc(num-1)+fibFunc(num-2)
+        return fib_func(num - 1) + fib_func(num - 2)
+
 
 # функция проверяет является ли число простым
-def isSimpleNum(num):
+def is_simple_num(num):
     i = 2
     while i < num:
         if num % i == 0:
@@ -16,14 +16,9 @@ def isSimpleNum(num):
     return True
 
 
-with open('in_nums.txt','r') as file:
-    with open('out_nums.txt','w') as outp:
+with open('in_nums.txt', 'r') as file:
+    with open('out_nums.txt', 'w') as outp:
         for line in file:
-            # проверка числа
-            if isSimpleNum(int(line)):
-                # последняя цифра
-                last_num=int(line.rstrip()[-1])
-                # находим число фибоначчи,преобразуем его в строку и пишем в файл
-                outp.write(str(fibFunc(last_num)) + '\n')
-
-
+            if is_simple_num(int(line)):
+                last_num = int(line.rstrip()[-1])
+                outp.write(str(fib_func(last_num)) + '\n')
